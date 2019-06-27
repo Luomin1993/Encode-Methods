a7203bc75704a2a0b12ed117db5ab817836611a1
Luo minzhong <879671933@qq.com>
2019-06-27 17:02:05 +0800

    Add files via upload

---
+++ fw.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import numpy as np;
+
+# 编码模块;
+def encode():
+    pass;
+
+# 解码模块;
+def decode():
+    pass;
+
+# 计算误差模块;
+def error():
+    pass;
+
+# 求梯度模块;
+def grad():
+    pass;
+
+# 更新参数模块;
+def update():
+    pass;
+
+
+# 主流程;
+def main():
+    pass;
